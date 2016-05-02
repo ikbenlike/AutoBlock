d50a24d77e06aac778d216407e7cbfcf476a1b37
thewatcher <thewatcheriswatchinu@gmail.com>
2016-05-02 15:19:05 +0200

    stops it from blocking users who are already blocked

--- anti-cancer-name.py
+++ anti-cancer-name.py
@@ -23,15 +23,27 @@ elif logInFromFile == True:
 	with open("inpassword.txt") as inpassword:
 		inPassword = intoken.read().replace("\n", "")
 
+blockFile = open("blocklist.txt", "r")
+blocked = blockFile.read().replace('\n', '')
+blockFile.close()
+
 payload = {"type": 2}
 async def block(userToBlock, userToBlockName, server, channel):
+	global blocked
 	headers = {"Content-Type": "application/json", "Authorization": client.token, "Origin": "https://discordapp.com", "Accept": "*/*", "Referer": "https://discordapp.com/channels/" + server + "/" + channel}
 	try:
-		with aiohttp.ClientSession() as session:
-			await asyncio.sleep(5)
-			async with session.put("https://discordapp.com/api/users/@me/relationships/" + userToBlock, data=json.dumps(payload), headers=headers) as response:
-				print("blocked " + userToBlock + " - " + userToBlockName)
-				await session.close()
+		if userToBlock not in blocked:
+			with aiohttp.ClientSession() as session:
+				await asyncio.sleep(5)
+				async with session.put("https://discordapp.com/api/users/@me/relationships/" + userToBlock, data=json.dumps(payload), headers=headers) as response:
+					print("blocked " + userToBlock + " - " + userToBlockName)
+					await session.close()
+					blockFile = open("blocklist.txt", "a")
+					blockFile.write(userToBlock)
+					blockFile.close()
+					blockFile = open("blocklist.txt", "r")
+					blocked = blockFile.read().replace('\n', '')
+					blockFile.close()
 	except aiohttp.errors.ServerDisconnectedError:
 		pass
 
@@ -41,7 +53,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
 	print("logged in as " + client.user.name + " - " + client.user.id)
-	print(client.token)
 
 init = False
 allowedChars = "aàãáäâbcdeèẽéëêfghiìĩíïîjklmnoòõóöôpqrsśtuùũúüûvǜṽǘwxẍyỳỹýÿŷzAÀÃÁÄBCDEÈẼËÉFGHIÌĨÍÏJKLMNOÒÕÓÖPQRSŚTUÙŨÚÜVǛṼǗWXẌYỲỸÝŸZ 1234567890 !@#$%*()_+`~[]}{\|;´\";:,./<>?'"
@@ -63,7 +74,6 @@ async def on_message(message):
 		tempAuthor = list(message.author.name)
 		for char in tempAuthor:
 			if char not in allowedChars:
-				print(char)
 				count += 1
 			if count > 2:
 				await block(message.author.id, message.author.name, message.server.id, message.channel.id)
